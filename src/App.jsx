4ba48f922b803b973bc9d5a3f7a667b5842f862a
prathamesh <prathameshsk94@gmail.com>
2024-06-27 11:59:04 +0530

    remove project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import Hero from "./components/Hero";
-import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className=" bg-zinc-950">
       <Hero />
-      <Projects />
     </div>
   );
 }
